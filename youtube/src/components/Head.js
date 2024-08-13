26dc1b96b68d53036306719fd652e93237d411ba
Ramanand <yram9757@gmail.com>
2024-08-13 14:45:22 +0530

    added redux Store in our project

--- youtube/src/components/Head.js
+++ youtube/src/components/Head.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Head = () => {
   return (
-    <div className='grid grid-flow-col'>
-      <div className='flex'>
+    <div className='grid grid-flow-col p-5 m-2 shadow-lg'>
+      <div className='flex col-span-1 '>
       <img 
       className='h-8'
       alt='menu' src='https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp&w=256'/>
       <img 
-      className='h-8'
+      className='h-8 mx-2'
       alt='logo' src='https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6.jpg'/>   
       </div>
-      <div>
-      <input type='text'/>
-      <button>Search</button>
+      <div className='col-span-10  px-10'>
+      <input className='w-1/2 border border-gray-400 p-2 rounded-l-full' type='text'/>
+      <button className='border border-gray-400 p-2 rounded-r-full'>Search</button>
       </div>
-      <div>
+      <div className='col-span-1'>
         <img alt='user'
         className='h-8' src='https://t3.ftcdn.net/jpg/05/53/79/60/360_F_553796090_XHrE6R9jwmBJUMo9HKl41hyHJ5gqt9oz.jpg'/>  
         </div>
--- youtube/src/components/Sidebar.js
+++ youtube/src/components/Sidebar.js
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='p-5 w-48 shadow-lg'>
+       <h1 className='font-bold'>SubsCriptions</h1>
+      <ul>
+        <li>Subscription</li>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+    <h1 className='font-bold pt-5'>SubsCriptions</h1>
+      <ul>
+        <li>Subscription</li>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+      <h1 className='font-bold pt-5'>SubsCriptions</h1>
+      <ul>
+        <li>Subscription</li>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+    </div>
   )
 }
 
