bbd6dd4d7300ed915b06e4f8c56bad85ca810611
Ramanand <yram9757@gmail.com>
2024-08-26 20:18:49 +0530

    data is not passing in video

--- youtube/src/components/VideoContainer.js
+++ youtube/src/components/VideoContainer.js
@@ -1,20 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { YOUTUBE_VIDEOS_API } from '../utils/constant'
+import VideoCard from './VideoCard'
 
 const VideoContainer = () => {
 
+  const [videos,setVideos]=useState([])
+
   useEffect(()=>{
     getVideos()
+    console.log(videos);
+    
   },[])
 
-  const getVideos=async()=>{
-    const data=await fetch(YOUTUBE_VIDEOS_API)
-    const json= await data.json()
-    console.log(json);
-    
-  }
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    console.log('Fetched videos:', json);
+    setVideos(json);
+  };
+  
   return (
-    <div>VideoContainer</div>
+    <div>
+      {/* <VideoCard info={videos[0]}/> */}
+    </div>
   )
 }
 
