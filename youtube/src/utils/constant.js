088f37d5807fa62df1a6321d33feb2dcdf7a2ada
Ramanand <yram9757@gmail.com>
2024-08-26 19:36:04 +0530

    added youtube api

---
+++ youtube/src/utils/constant.js
@@ -0,0 +1,2 @@
+const GOOGLE_API_KEY="AIzaSyCoKmCKMezu6_VEYPqJLm2quegQ1PY05VQ"
+export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+GOOGLE_API_KEY
