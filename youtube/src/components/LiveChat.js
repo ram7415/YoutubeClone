06b86649778da560de0a3bb90462c618f3829096
Ramanand <yram9757@gmail.com>
2024-09-25 17:39:18 +0530

    added live comments

---
+++ youtube/src/components/LiveChat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LiveChat = () => {
+  return (
+    <div className='w-fill h-[600px] ml-2 p-2  border border-black'>LiveChat</div>
+  )
+}
+
+export default LiveChat
