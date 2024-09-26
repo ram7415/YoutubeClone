387cd0905f61efeb60c72905f55f5f2084e283ce
Ramanand <yram9757@gmail.com>
2024-09-26 20:02:07 +0530

    added chatMessaage

--- youtube/src/components/LiveChat.js
+++ youtube/src/components/LiveChat.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const LiveChat = () => {
   return (
-    <div className='w-fill h-[600px] ml-2 p-2  border border-black'>LiveChat</div>
+    <div className='w-fill h-[600px] ml-2 p-2  border border-black bg-slate-100 rounded-lg'>
+        LiveChat</div>
   )
 }
 
