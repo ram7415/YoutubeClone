9733292fede4bd352d8fe48fc963c7a1b0e0f7a5
Ramanand <yram9757@gmail.com>
2024-09-27 11:14:35 +0530

    added chatSlice in redux store

--- youtube/src/components/LiveChat.js
+++ youtube/src/components/LiveChat.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ChatMessage from './ChatMessage'
 
 const LiveChat = () => {
+
+useEffect(()=>{
+  const i=setInterval(()=>{
+    //api pollling
+  },2000)
+  return()=>clearInterval(i)
+})
+
   return (
     <div className='w-fill h-[600px] ml-2 p-2  border border-black bg-slate-100 rounded-lg'>
         <ChatMessage name='Ram' message='hello from chat'/></div>
