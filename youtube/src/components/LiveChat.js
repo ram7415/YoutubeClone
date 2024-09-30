d784fdb5ac874f49b9d8cb00e9c6abcbb3ddcc2c
Ramanand <yram9757@gmail.com>
2024-09-30 11:01:50 +0530

    added live comment

--- youtube/src/components/LiveChat.js
+++ youtube/src/components/LiveChat.js
@@ -1,18 +1,33 @@
 import React, { useEffect } from 'react'
 import ChatMessage from './ChatMessage'
+import { useDispatch, useSelector } from 'react-redux'
+import { addMessage } from '../utils/chatSlice';
 
 const LiveChat = () => {
 
+  const chatMessages=useSelector((store)=>store.chat.messages)
+  const disptach=useDispatch();
 useEffect(()=>{
   const i=setInterval(()=>{
     //api pollling
+    disptach(
+      addMessage({
+        name:"Ram",
+        message:"this is the msg from live"
+      })
+    )
   },2000)
   return()=>clearInterval(i)
 })
 
   return (
     <div className='w-fill h-[600px] ml-2 p-2  border border-black bg-slate-100 rounded-lg'>
-        <ChatMessage name='Ram' message='hello from chat'/></div>
+       {
+       chatMessages.map((c,i)=>(<ChatMessage
+        name={c.name} key={i}
+        message={c.message}
+      /> ))}
+       </div>
   )
 }
 
